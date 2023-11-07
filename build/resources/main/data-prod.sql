8e273b6bd5fe406412207f699c92ed41589779bb
HYEMI YUN <yhm54321@naver.com>
2023-11-07 19:08:01 +0900

    final

--- build/resources/main/data-prod.sql
+++
@@ -1,5 +0,0 @@
-delete from RtoU.characterinfo;
-
-insert into RtoU.characterinfo (name, voicename, pitch) values ('Huindungi', 'en-US-Neural2-E', 13.4);
-insert into RtoU.characterinfo (name, voicename, pitch) values ('Jjijjigi', 'en-US-Neural2-A', 16.8);
-insert into RtoU.characterinfo (name, voicename, pitch) values ('Mongmongi', 'en-US-Neural2-I', 16.8);
