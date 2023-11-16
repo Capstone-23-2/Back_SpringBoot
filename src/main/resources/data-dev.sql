292e472c56091e5a32ca1091d0a68638fb71e39b
HYEMI YUN <yhm54321@naver.com>
2023-11-16 17:54:34 +0900

    modify

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -1,5 +1,5 @@
 delete from rtou.characterinfo;
 
 insert into rtou.characterinfo (name, voicename, pitch, langcode) values ('Hindung-e', 'en-US-Neural2-G', 13.6, "en-US");
-insert into rtou.characterinfo (name, voicename, pitch, langcode) values ('Jjijjigi', 'en-US-Neural2-A', 16.8, "en-US");
+insert into rtou.characterinfo (name, voicename, pitch, langcode) values ('Yangyangi', 'en-US-Neural2-D', 8.8, "en-US");
 insert into rtou.characterinfo (name, voicename, pitch, langcode) values ('Mongmong-e', 'en-AU-Neural2-B', 12.0, "en-AU");
